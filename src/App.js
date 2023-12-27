b9e87352048073fe293e2b43cbb839295b28a8e1
Igor_LAO <igoli6767@gmail.com>
2023-12-26 21:48:00 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.scss';
+import { useEffect, useState } from 'react';
 
 import yondu from '../src/pages/assets/pais/y.jpg';
 import marlin from '../src/pages/assets/pais/marlin.png';
@@ -29,8 +31,6 @@ import dupla from '../src/pages/assets/juntos/dupla.png'
 import corretSimbolo from '../src/pages/assets/correto.png'
 import erradoSimbolo from '../src/pages/assets/errado.png'
 
-import './App.scss';
-import { useEffect, useState } from 'react';
 
 function App() {
   const [valor1, setValor1] = useState('')
@@ -45,7 +45,7 @@ function App() {
       if (valor1 !== '' && valor2 !== '') {
         if (valor1 === valor2) {
           setCorreto(valor1);
-          
+
           setCerto('certo')
 
           setTimeout(() => {
@@ -54,19 +54,19 @@ function App() {
 
         }
 
-          else if(valor1 != valor2){
+        else if (valor1 != valor2) {
           setCorreto(valor1);
-          
+
           setErrado('errado')
 
           setTimeout(() => {
             window.location.reload();
           }, 1000);
 
-          }
+        }
       }
     };
-  
+
     Validação();
   }, [valor1, valor2]);
 
@@ -85,48 +85,48 @@ function App() {
 
 
   return (
-      <div className='main'>
-        
-          <h1> Quem é o pai de quem? </h1>
-        <div className='pais'>
-          <img src={marlin} onClick={ () => setValor1('Marlin_Nemo') }  />
-          <img src={dartVader} onClick={() => setValor1('Dartvader_Luke')}  />
-          <img src={homer} onClick={() => setValor1('Homer_Bart')}  />
-          <img src={yondu} onClick={() => setValor1('Yondu_quill')} />
-          <img src={mufasa} onClick={() => setValor1('mufasa_Simba')} />
-          <img src={rick} onClick={() => setValor1('rick_Carl')} />
-          <img src={srincrivel} onClick={() => setValor1('SrIncrivel_Zeze')} alt='Sr. Incrível' />
-          <img src={paiDoMundo} onClick={() => setValor1('dupla')} alt='Sr. Incrível' />
+    <div className='main'>
 
-        </div>
+      <h1 style={{fontSize: '45px', color: '#6FC17C', fontFamily: "'Courier New', Courier, monospace'"}}> Quem é o pai de quem? </h1>
+      <div className='pais'>
+        <img src={marlin} onClick={() => setValor1('Marlin_Nemo')} />
+        <img src={dartVader} onClick={() => setValor1('Dartvader_Luke')} />
+        <img src={homer} onClick={() => setValor1('Homer_Bart')} />
+        <img src={yondu} onClick={() => setValor1('Yondu_quill')} />
+        <img src={mufasa} onClick={() => setValor1('mufasa_Simba')} />
+        <img src={rick} onClick={() => setValor1('rick_Carl')} />
+        <img src={srincrivel} onClick={() => setValor1('SrIncrivel_Zeze')} alt='Sr. Incrível' />
+        <img src={paiDoMundo} onClick={() => setValor1('dupla')} alt='Sr. Incrível' />
 
-              <span className='imagemCorreto'>
-                <span>
-                  <span className={certo}>
-                <img className='imgjuntos' src={imagens[correto]} />
-                  <div className={certo + '2'}>
-                  <img src={corretSimbolo} className='CorretSimbolo'  />
-                  </div>
-                  </span>
+      </div>
 
-                  <div className={errado}>
-                  <img src={erradoSimbolo}  />
-                  </div>
-                </span>
-              </span>
+      <span className='imagemCorreto'>
+        <span>
+          <span className={certo}>
+            <img className='imgjuntos' src={imagens[correto]} />
+            <div className={certo + '2'}>
+              <img src={corretSimbolo} className='CorretSimbolo' />
+            </div>
+          </span>
 
-        <div className='filhos'>
-          <img src={quill} onClick={() => setValor2('Yondu_quill')}  />
-          <img src={simba} onClick={() => setValor2('mufasa_Simba')} />
-          <img src={zeze}   onClick={() => setValor2('SrIncrivel_Zeze')}/>
-          <img src={carl} onClick={() => setValor2('rick_Carl')} />
-          <img src={MelhorDev} onClick={() => setValor2('dupla')}  />
-          <img src={nemo} onClick={ () => setValor2('Marlin_Nemo') }  />
-          <img src={luke_skywalker}  onClick={() => setValor2('Dartvader_Luke')} />
-          <img src={bart} onClick={() => setValor2('Homer_Bart')}  />
-        </div>
+          <div className={errado}>
+            <img src={erradoSimbolo} />
+          </div>
+        </span>
+      </span>
 
-   </div>
+      <div className='filhos'>
+        <img src={quill} onClick={() => setValor2('Yondu_quill')} />
+        <img src={simba} onClick={() => setValor2('mufasa_Simba')} />
+        <img src={zeze} onClick={() => setValor2('SrIncrivel_Zeze')} />
+        <img src={carl} onClick={() => setValor2('rick_Carl')} />
+        <img src={MelhorDev} onClick={() => setValor2('dupla')} />
+        <img src={nemo} onClick={() => setValor2('Marlin_Nemo')} />
+        <img src={luke_skywalker} onClick={() => setValor2('Dartvader_Luke')} />
+        <img src={bart} onClick={() => setValor2('Homer_Bart')} />
+      </div>
+
+    </div>
   );
 }
 
